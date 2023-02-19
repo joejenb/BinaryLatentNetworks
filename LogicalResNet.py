96a928e4c6c2bdbdc228d18d035a93cfd00fc112
Joe <joejbailey@icloud.com>
2023-02-19 19:25:06 +0000

    Initialise weights normaly around 0.5 with a variance of 0.5

--- LogicalResNet.py
+++ LogicalResNet.py
@@ -15,8 +15,8 @@ class LogicalResNet(ResNet):
                  groups, width_per_group, replace_stride_with_dilation, norm_layer)
 
         self.device = device
-        logical_body = [nn.Linear(num_features // (depth+1), num_features // (depth+2), bias=False) for depth in range(tree_depth - 1)]
-        logical_head = [nn.Linear(num_features // tree_depth, num_classes, bias=False)]
+        logical_body = [torch.nn.init.normal_(nn.Linear(num_features // (depth+1), num_features // (depth+2), bias=False), mean=0.5, std=0.5) for depth in range(tree_depth - 1)]
+        logical_head = [torch.nn.init.normal_(nn.Linear(num_features // tree_depth, num_classes, bias=False), mean=0.5, std=0.5)]
         self.logical_tree = nn.ModuleList(logical_body + logical_head)
         
     def forward(self, x):
